267f362e669e51c983614d9f214a2ba79bfbdb51
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2025-06-07 17:35:06 +0530

    Initial commit

---
+++ src/main/java/com/shubhajit/genai/chatbot/GenaiChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.shubhajit.genai.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaiChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenaiChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubhajit/genai/chatbot/GenaiChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhajit.genai.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenaiChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
